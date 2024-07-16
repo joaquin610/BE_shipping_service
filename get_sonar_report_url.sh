4f571e89a4386595d61bbab19dbe42b93b816351
joaquin610 <77033562+joaquin610@users.noreply.github.com>
2024-07-15 22:20:45 -0300

    Create get_sonar_report_url.sh

---
+++ get_sonar_report_url.sh
@@ -0,0 +1,4 @@
+PROJECT_KEY="joaquin610_BE_products_service"
+ORGANIZATION_KEY="joaquin610"
+REPORT_URL="https://sonarcloud.io/dashboard?id=${PROJECT_KEY}"
+echo "SonarCloud analysis report is available at: ${REPORT_URL}"
